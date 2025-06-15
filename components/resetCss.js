b2c9239147c573b3e87dff9fe59e0d9263c6d468
ElStron <luisstron45@gmail.com>
2025-06-15 00:12:50 -0600

    remove unused components and styles from the application

--- components/resetCss.js
+++
@@ -1,9 +0,0 @@
-export const globalResetStyles = new CSSStyleSheet();
-globalResetStyles.replaceSync(`
-  *, *:before, *:after {
-    box-sizing: border-box;
-    margin: 0;
-    padding: 0;
-    text-decoration: none;
-  }
-`);
